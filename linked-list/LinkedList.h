4b94be3e6df2937a7a90ad78ad8b38ae7489047f
Bora Erinc <bora17erinc@gmail.com>
2025-08-30 18:06:25 +0100

    work on singly linked list
    - add copy constructor, assignment operator, move semantics
    - add front/push_front methods
    - add back method
    
    to-do:
    - fix back method
    - fix Node class, should be POD struct
    - fix move assignment operator
    - refactor linked list clean up into function

--- linked-list/LinkedList.h
+++ linked-list/LinkedList.h
@@ -4,73 +4,97 @@
 // or should it just be the node itself?
 // If it has a head, then should that head act as padding,
 // holding no data but just pointing to the first element?
-// Should Nodes have move semantics?
+// Should LinkedListNodes have move semantics?
 // How do you handle something like `T data` in a move oeprator?
 // You can't set values to nullptr, nor can you call a destructor on them.
+template <typename T>
+class LinkedListNode {
+  T data;
+  LinkedListNode* next;
+
+public:
+  LinkedListNode(): next(nullptr) {}
+
+  LinkedListNode(const T& val, LinkedListNode* ptr = nullptr): data(val), next(ptr) {}
+
+  LinkedListNode(const LinkedListNode& rhs) : data(rhs.data), next(rhs.next) {}
+
+  LinkedListNode& operator=(const LinkedListNode& rhs) {
+    if(this == &rhs) return *this;
+
+    data = rhs.data;
+    next = rhs.next;
+
+    return *this;
+  }
+
+  ~LinkedListNode() {}
+
+  LinkedListNode(const LinkedListNode&& rhs) noexcept : data(rhs.data), next(rhs.next) {
+    rhs.next = nullptr;
+  }
+
+  LinkedListNode& operator=(const LinkedListNode&& rhs) noexcept {
+    if(this == &rhs) return *this;
+
+    data = rhs.data;
+    next = rhs.next;
+
+    rhs.next = nullptr;
+
+    return *this;
+  }
+
+  void setNext(LinkedListNode* ptr) noexcept {
+    next = ptr;
+  }
+
+  void setVal(const T& val) noexcept {
+    data = val;
+  }
+
+  LinkedListNode* getNext(LinkedListNode* ptr) noexcept {
+    return ptr;
+  }
+
+  T getVal(const T& val) noexcept {
+    return val;
+  }
+};
 
 template <typename T>
 class LinkedList {
 
-  class Node {
-    T data;
-    Node* next;
-
-  public:
-    Node(): next(nullptr) {}
-
-    Node(const T& val, Node* ptr = nullptr): data(val), next(ptr) {}
-
-    Node(const Node& rhs) : data(rhs.data), next(rhs.next) {}
-
-    Node& operator=(const Node& rhs) {
-      if(this == &rhs) return *this;
-
-      data = rhs.data;
-      next = rhs.next;
-
-      return *this;
-    }
-
-    ~Node() {}
-
-    Node(const Node&& rhs) noexcept : data(rhs.data), next(rhs.next) {
-      rhs.next = nullptr;
-    }
-
-    Node& operator=(const Node&& rhs) noexcept {
-      if(this == &rhs) return *this;
-
-      data = rhs.data;
-      next = rhs.next;
-
-      rhs.next = nullptr;
-
-      return *this;
-    }
-
-    void setNext(Node* ptr) noexcept {
-      next = ptr;
-    }
-
-    void setVal(const T& val) noexcept {
-      data = val;
-    }
-
-    Node* getNext(Node* ptr) noexcept {
-      return ptr;
-    }
-
-    T getVal(const T& val) noexcept {
-      return val;
-    }
-  };
-
-  Node* head;
+  LinkedListNode<T>* head;
+  size_t m_size;
 
 public:
   
-  LinkedList() : head(new Node()) { }
+  LinkedList<T>() : head(new LinkedListNode<T>()), m_size(0) { }
+  
+  void push_front(const T& val) {
+    LinkedListNode<T>* curr = new LinkedListNode(val, head);
+    head = curr;
+    ++m_size;
+  }
 
+  void pop_front() {
+    if(head->next == nullptr) return;
+    LinkedListNode<T>* curr = head->next->next;
+    delete head->next;
+    head->next = curr;
+    --m_size;
+  }
+
+  size_t size() const noexcept { return m_size;}
+
+  bool empty() const noexcept { return m_size == 0; }
+
+  const T& front() const {
+    if(!empty()) {
+      return *(head->next);
+    }
+  }
 
 };
 
